3d9d751a6b17362ee688027f63ca9f902f5ea67d
osbowski <61865334+osbowski@users.noreply.github.com>
2021-12-18 13:59:03 +0100

    clean bookReducer and init store

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,22 @@
+import { Dispatch } from 'redux'
+import { ActionType } from "../action-types/index";
+import { Book } from "../../../types";
+import { Action } from '../actions/index'
+
+export const addBookToCart = (book: Book) => {
+  return (dispatch:Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.ADD_BOOK,
+      payload:book
+    });
+  };
+};
+
+export const removeBookFromCart = (id:number)=>{
+    return (dispatch:Dispatch<Action>) =>{
+        dispatch({
+            type:ActionType.REMOVE_BOOK,
+            payload:id
+        })
+    }
+}
--- src/state/reducers/bookReducer.ts
+++ src/state/reducers/bookReducer.ts
@@ -2,14 +2,19 @@ import { Book } from "../../../types";
 import { ActionType } from "../action-types";
 import { Action } from '../actions';
 
-const initialState:Book[]=[]
-const booksReducer = (state:Book[]=initialState,action:Action)=>{
+interface BooksState{
+    books:Book[]
+}
+const initialState={
+    books:[]
+}
+const booksReducer = (state:BooksState=initialState,action:Action)=>{
     switch(action.type){
         case ActionType.ADD_BOOK:
-            state.push(action.payload)
+            state.books.push(action.payload)
             return state;
         case ActionType.REMOVE_BOOK:
-            return state.filter(book=>book.id !== action.payload)
+            return state.books.filter(book=>book.id !== action.payload)
             
         
         default:
