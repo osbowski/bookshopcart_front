fe59fe4348054d1fd917ae24b49acac83e3eaee9
osbowski <61865334+osbowski@users.noreply.github.com>
2021-12-19 20:58:13 +0100

    Get data from api error handling

--- src/components/books/BookList.tsx
+++ src/components/books/BookList.tsx
@@ -5,37 +5,49 @@ import { Book } from "../../../types";
 import DataService from "../../service/DataService";
 import BookListElement from "./BookListElment";
 
-interface BookListProps{
-    isInCart:boolean;
+interface BookListProps {
+  isInCart: boolean;
 }
-const BookList:React.FC<BookListProps> = ({isInCart})=>{
-    const [books,setBooks] = useState<Book[]>([]);
-    const store = useSelector((state:RootState)=>state.books)
-    useEffect(()=>{
-        if(isInCart){
-           setBooks(store.books)
-        }else{
-            const getBooks = async ()=>{
-                const data = await DataService.getAll()
-                const fetchedBooks =data.data;
-                setBooks(fetchedBooks.data);
-            }
-            getBooks()
+const BookList: React.FC<BookListProps> = ({ isInCart }) => {
+  const [books, setBooks] = useState<Book[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const store = useSelector((state: RootState) => state.books);
+  useEffect(() => {
+    if (isInCart) {
+      setBooks(store.books);
+    } else {
+      const getBooks = async () => {
+        try {
+          const data = await DataService.getAll();
+          const fetchedBooks = data.data;
+          setBooks(fetchedBooks.data);
+        } catch {
+          setError("Something going wrong, try again later");
         }
+      };
+      getBooks();
+    }
+  }, [store.books]);
 
-    },[store.books])
-
-    return(
-        <>
+  return (
+    <>
+      {error ? (
+        <p>{error}</p>
+      ) : (
         <ul>
-            {books.map((book:Book)=>{
-                return(
-                    <BookListElement key={book.id} bookData={book} isInCart={isInCart}/>
-                )
-            })}
+          {books.map((book: Book) => {
+            return (
+              <BookListElement
+                key={book.id}
+                bookData={book}
+                isInCart={isInCart}
+              />
+            );
+          })}
         </ul>
+      )}
     </>
-    )
-}
+  );
+};
 
 export default BookList;
